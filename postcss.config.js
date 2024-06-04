52ceea426e4bda27f347c05d66f623f59fc0eedc
DarksideEvil <sinister58450@gmail.com>
2024-06-04 19:08:15 +0500

    intial & last commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
 export default {
   plugins: {
-    tailwindcss,
-    autoprefixer,
-    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+    tailwindcss: {},
+    autoprefixer: {},
+    // ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
 }
